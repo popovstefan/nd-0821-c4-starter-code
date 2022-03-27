3787a7166722b0e8f4f692ce86e732bb4018b0e0
Stefan Popov <popovstefan@live.com>
2022-03-27 16:50:07 +0200

    Step 2: Training, Scoring, and Deployment

--- deployment.py
+++ deployment.py
@@ -1,27 +1,30 @@
-from flask import Flask, session, jsonify, request
-import pandas as pd
-import numpy as np
-import pickle
-import os
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
 import json
+import os
+
+# Load config.json and correct path variable
+import shutil
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+dataset_csv_path = os.path.join(config['output_folder_path'])
+model_path = os.path.join(config["output_model_path"])
+prod_deployment_path = os.path.join(config['prod_deployment_path'])
+if not os.path.exists(prod_deployment_path):
+    os.makedirs(prod_deployment_path)
+
+# Function for deployment
+def store_model_into_pickle():
+    """
+    Copy the latest pickle file, the latestscore.txt value, and the ingestfiles.txt file into the deployment directory
+    """
+    shutil.copy2(src=os.path.join(dataset_csv_path, "ingestedfiles.csv"),
+                 dst=os.path.join(prod_deployment_path, "ingestedfiles.csv"))
+    shutil.copy2(src=os.path.join(model_path, "trainedmodel.pkl"),
+                 dst=os.path.join(prod_deployment_path, "trainedmodel.pkl"))
+    shutil.copy2(src=os.path.join(model_path, "latestscore.txt"),
+                 dst=os.path.join(prod_deployment_path, "latestscore.txt"))
 
 
-
-##################Load config.json and correct path variable
-with open('config.json','r') as f:
-    config = json.load(f) 
-
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-prod_deployment_path = os.path.join(config['prod_deployment_path']) 
-
-
-####################function for deployment
-def store_model_into_pickle(model):
-    #copy the latest pickle file, the latestscore.txt value, and the ingestfiles.txt file into the deployment directory
-        
-        
-        
-
+if __name__ == "__main__":
+    store_model_into_pickle()
--- scoring.py
+++ scoring.py
@@ -1,25 +1,40 @@
-from flask import Flask, session, jsonify, request
-import pandas as pd
-import numpy as np
-import pickle
-import os
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
 import json
+import os
+
+import joblib
+import pandas as pd
+# Load config.json and get path variables
+from sklearn import metrics
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+dataset_csv_path = os.path.join(config['output_folder_path'])
+test_data_path = os.path.join(config['test_data_path'])
+model_path = os.path.join(config["output_model_path"])
 
 
-
-#################Load config.json and get path variables
-with open('config.json','r') as f:
-    config = json.load(f) 
-
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-test_data_path = os.path.join(config['test_data_path']) 
-
-
-#################Function for model scoring
+# Function for model scoring
 def score_model():
-    #this function should take a trained model, load test data, and calculate an F1 score for the model relative to the test data
-    #it should write the result to the latestscore.txt file
+    """
+    This function should take a trained model, load test data,
+     and calculate an F1 score for the model relative to the test data.
+    It should write the result to the latestscore.txt file
+    """
+    # load data
+    df = pd.read_csv(os.path.join(test_data_path, "testdata.csv"))
+    y = df["exited"]
+    X = df.drop(columns=["exited", "corporation"])
+    # load model
+    model = joblib.load(os.path.join(model_path, "trainedmodel.pkl"))
+    # predict on test data
+    y_pred = model.predict(X)
+    # score prediction
+    f1 = metrics.f1_score(y, y_pred)
+    # save the result
+    with open(os.path.join(model_path, "latestscore.txt"), "w") as f:
+        f.write("%s\n" % f1)
 
+
+if __name__ == "__main__":
+    score_model()
